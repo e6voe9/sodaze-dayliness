0a9b0e7aa700533d2a264c2ccaae06c51f9878dd
e6voe9 <george.bakhanovych@gmail.com>
2021-07-24 20:56:08 +0300

    main screen heading line

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ buyButtons.forEach((e) => {
 		document.body.style.overflow = "hidden";
 	});
 });
-const closeModalButtons = document.querySelectorAll(".modal-form__bg");
-closeModalButtons.forEach((e) => {
+const closeModalBgs = document.querySelectorAll(".modal-form__bg");
+closeModalBgs.forEach((e) => {
 	e.addEventListener("click", () => {
 		const currentModal = e.parentElement;
 		setTimeout(() => {
@@ -21,3 +21,28 @@ closeModalButtons.forEach((e) => {
 		currentModal.classList.add("modal-form--close");
 	});
 });
+const closeModalButtons = document.querySelectorAll(".modal-form__close-button");
+closeModalButtons.forEach((e) => {
+	e.addEventListener("click", () => {
+		const currentModal = e.parentElement.parentElement;
+		setTimeout(() => {
+			currentModal.classList.remove("modal-form--open");
+			currentModal.classList.remove("modal-form--close");
+			document.body.style.overflow = "visible";
+		}, 300);
+		currentModal.classList.add("modal-form--close");
+	});
+});
+
+
+const mainVideo = document.querySelector(".main-screen__video");
+const musicButton = document.querySelector(".music-button");
+
+musicButton.addEventListener("click", function () {
+	mainVideo.muted = !mainVideo.muted;
+	musicButton.classList.toggle('music-button--active')
+});
+
+
+
+
